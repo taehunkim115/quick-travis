54909aee0e5b3481dbc13e37354ccd9eb6e1258a
taehunkim115 <wlehwk115@gmail.com>
2019-12-12 00:41:48 -0600

    cypress content test done

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -3,4 +3,10 @@ describe ('Test App', () => {
   it ('launches', () => {
     cy.visit ('/');
   });
+
+  it ('restaurant card opens properly', () => {
+    cy.visit ('/');
+    cy.get('[data-cy=restaurant]').should('contain', 'Lunch, Dinner Available');
+    cy.get('[data-cy=restaurantname]').should('contain', 'Koco Table');
+  });
 });
